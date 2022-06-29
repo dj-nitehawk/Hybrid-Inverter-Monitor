767e2b7b438db9f029b7cdae086ac44616778d15
Damith G <djnitehawk83@gmail.com>
2022-06-29 10:54:55 +0530

    wip
    wip

---
+++ SandBox/Inverter/Inverter.cs
@@ -0,0 +1,18 @@
+﻿namespace InverterMon.Inverter;
+
+internal static class Inverter
+{
+    public static Status Status { get; } = new();
+
+    static Inverter()
+    {
+        try
+        {
+            CommandBase.Port = File.Open("/dev/hidraw0", FileMode.Open, FileAccess.ReadWrite);
+        }
+        catch
+        {
+            throw new Exception("Make sure the inverter is connected via USB port!!!");
+        }
+    }
+}
--- SandBox/Inverter/Status.cs
+++ SandBox/Inverter/Status.cs
@@ -1,54 +1,14 @@
-﻿public class Status
+﻿namespace InverterMon.Inverter;
+
+internal class Status : CommandBase
 {
-    private int loadWatt;
-    private decimal loadWattHour;
-    private int pvInputWatt;
-    private DateTime lastloadWattHourComputed;
-    private decimal pvInputWattHour;
-    private DateTime lastpvInputWattHourComputed;
+    protected override string Command => "QPIGS";
 
-    public void Parse(string rawData)
-    {
-        string[] dataElem = rawData[1..].Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-        GridVoltage = Convert.ToDecimal(dataElem[0]);
-        GridFrequency = Convert.ToDecimal(dataElem[1]);
-        OutputVoltage = Convert.ToDecimal(dataElem[2]);
-        OutputFrequency = Convert.ToDecimal(dataElem[3]);
-        LoadVA = int.Parse(dataElem[4]);
-        LoadWatt = int.Parse(dataElem[5]);
-        LoadPercentage = Convert.ToDecimal(dataElem[6]);
-        BusVoltage = Convert.ToDecimal(dataElem[7]);
-        BatteryVoltage = Convert.ToDecimal(dataElem[8]);
-        BatteryChargeCurrent = int.Parse(dataElem[9]);
-        BatteryCapacity = int.Parse(dataElem[10]);
-        HeatSinkTemperature = int.Parse(dataElem[11]);
-        PVInputCurrent = Convert.ToDecimal(dataElem[12]);
-        PVInputVoltage = Convert.ToDecimal(dataElem[13]);
-        SCCVoltage = Convert.ToDecimal(dataElem[14]);
-        BatteryDischargeCurrent = int.Parse(dataElem[15]);
-        PVOrACFeed = dataElem[16][0];
-        LoadOn = dataElem[16][3];
-        SCCOn = dataElem[16][1];
-        ACChargeOn = dataElem[16][2];
-        PVInputWatt = int.Parse(dataElem[19]);
-    }
-
-    //("V", "power-plug")]
     public decimal GridVoltage { get; private set; }
-
-    //("Hz", "current-ac")]
     public decimal GridFrequency { get; private set; }
-
-    //"V", "power-plug")]
     public decimal OutputVoltage { get; private set; }
-
-    //"Hz", "current-ac")]
     public decimal OutputFrequency { get; private set; }
-
-    //"VA", "chart-bell-curve")]
     public int LoadVA { get; private set; }
-
-    //("W", "chart-bell-curve")]
     public int LoadWatt
     {
         get => loadWatt;
@@ -62,8 +22,6 @@
             }
         }
     }
-
-    //("Wh", "chart-bell-curve")]
     public decimal LoadWattHour
     {
         get => loadWattHour;
@@ -77,32 +35,14 @@
             lastloadWattHourComputed = DateTime.Now;
         }
     }
-
-    //"%", "brightness-percent")]
     public decimal LoadPercentage { get; private set; }
-
-    //"V", "details")]
     public decimal BusVoltage { get; private set; }
-
-    //"V", "battery-outline")]
     public decimal BatteryVoltage { get; private set; }
-
-    //"A", "current-dc")]
     public int BatteryChargeCurrent { get; private set; }
-
-    //"%", "battery-outline")]
     public int BatteryCapacity { get; private set; }
-
-    //"°C", "details")]
     public int HeatSinkTemperature { get; private set; }
-
-    //"A", "solar-panel-large")]
     public decimal PVInputCurrent { get; private set; }
-
-    //"V", "solar-panel-large")]
     public decimal PVInputVoltage { get; private set; }
-
-    //"W", "solar-panel-large")]
     public int PVInputWatt
     {
         get => pvInputWatt; set
@@ -115,8 +55,6 @@
             }
         }
     }
-
-    //("Wh", "solar-panel-large")]
     public decimal PVInputWattHour
     {
         get => pvInputWattHour; set
@@ -129,22 +67,50 @@
 
         }
     }
-
-    //"V", "current-dc")]
     public decimal SCCVoltage { get; private set; }
-
-    //("A", "current-dc")]
     public int BatteryDischargeCurrent { get; private set; }
-
-    //"", "power")]
     public char PVOrACFeed { get; private set; }
-
-    //"", "power")]
     public char LoadOn { get; private set; }
-
-    //"", "power")]
     public char SCCOn { get; private set; }
-
-    //"", "power")]
     public char ACChargeOn { get; private set; }
+
+    private int loadWatt;
+    private decimal loadWattHour;
+    private int pvInputWatt;
+    private DateTime lastloadWattHourComputed;
+    private decimal pvInputWattHour;
+    private DateTime lastpvInputWattHourComputed;
+
+    public override void Update()
+    {
+        base.Update();
+        if (RawResponse is null)
+        {
+            return;
+        }
+
+        string[]? parts = RawResponse[1..].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        GridVoltage = decimal.Parse(parts[0]);
+        GridFrequency = decimal.Parse(parts[1]);
+        OutputVoltage = decimal.Parse(parts[2]);
+        OutputFrequency = decimal.Parse(parts[3]);
+        LoadVA = int.Parse(parts[4]);
+        LoadWatt = int.Parse(parts[5]);
+        LoadPercentage = decimal.Parse(parts[6]);
+        BusVoltage = decimal.Parse(parts[7]);
+        BatteryVoltage = decimal.Parse(parts[8]);
+        BatteryChargeCurrent = int.Parse(parts[9]);
+        BatteryCapacity = int.Parse(parts[10]);
+        HeatSinkTemperature = int.Parse(parts[11]);
+        PVInputCurrent = decimal.Parse(parts[12]);
+        PVInputVoltage = decimal.Parse(parts[13]);
+        SCCVoltage = decimal.Parse(parts[14]);
+        BatteryDischargeCurrent = int.Parse(parts[15]);
+        PVOrACFeed = parts[16][0];
+        LoadOn = parts[16][3];
+        SCCOn = parts[16][1];
+        ACChargeOn = parts[16][2];
+        PVInputWatt = int.Parse(parts[19]);
+    }
 }
