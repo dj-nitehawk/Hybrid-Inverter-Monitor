a7bf16711c80060722274eefb90121a5b164be9d
Damith G <djnitehawk83@gmail.com>
2022-06-30 14:47:32 +0530

    dummy status data for debugging

--- src/Server/Endpoints/GetStatus/Endpoint.cs
+++ src/Server/Endpoints/GetStatus/Endpoint.cs
@@ -15,8 +15,17 @@ public class Endpoint : EndpointWithoutRequest<object>
         AllowAnonymous();
     }
 
-    public override Task HandleAsync(CancellationToken c)
-        => SendAsync(GetDataStream(c), cancellation: c);
+    public override async Task HandleAsync(CancellationToken c)
+    {
+        try
+        {
+            await SendAsync(GetDataStream(c), cancellation: c);
+        }
+        catch (TaskCanceledException)
+        {
+            //nothing to do here
+        }
+    }
 
     private async IAsyncEnumerable<InverterStatus> GetDataStream([EnumeratorCancellation] CancellationToken c)
     {
@@ -24,17 +33,28 @@ public class Endpoint : EndpointWithoutRequest<object>
 
         while (!c.IsCancellationRequested)
         {
-            cmd.IsComplete = false;
-            cmd.StartTime = DateTime.Now;
-            Queue.Commands.Enqueue(cmd);
-
-            while (!cmd.IsComplete && !cmd.TimedOut)
-                await Task.Delay(500, c);
-
-            if (cmd.IsComplete)
+            if (Env.IsDevelopment())
+            {
+                await Task.Delay(3000, c);
+                cmd.Data.PVInputVoltage = Random.Shared.Next(300);
+                cmd.Data.BatteryVoltage = Random.Shared.Next(26);
+                cmd.Data.LoadWatts = Random.Shared.Next(3500);
                 yield return cmd.Data;
+            }
+            else
+            {
+                cmd.IsComplete = false;
+                cmd.StartTime = DateTime.Now;
+                Queue.Commands.Enqueue(cmd);
 
-            await Task.Delay(5000, c);
+                while (!cmd.IsComplete && !cmd.TimedOut)
+                    await Task.Delay(500, c);
+
+                if (cmd.IsComplete)
+                    yield return cmd.Data;
+
+                await Task.Delay(5000, c);
+            }
         }
     }
 }
