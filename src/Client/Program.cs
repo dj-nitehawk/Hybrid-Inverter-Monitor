7eb0653c8cbdb106a1c5d9a9235c301f12b5d8fd
Damith G <djnitehawk83@gmail.com>
2022-07-19 10:54:58 +0530

    add 3 sec timeout to web client

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -5,6 +5,9 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddScoped(_ => new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress),
+    Timeout = TimeSpan.FromSeconds(3)
+});
 await builder.Build().RunAsync();
--- src/changelog.md
+++ src/changelog.md
@@ -1,9 +1,2 @@
-## change log
-- user customizable settings (max pv capacity, pv logging start & end time)
-- ability to specify usb device path & web server port via `appsettings.json` file
-- display pv generation in `kWh`
-- responsive ui improvements
-- pv capacity & inverter load meters (progress bars) 
-- pv logging bucket size changed to 5mins
-- improve command queue processing logic
-- fix a couple of division by zero errors
+## changelog
+- add a 3 second timeout to http client
